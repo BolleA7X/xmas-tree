ae887cbfdc3c6a1ec5191afa27b19d781116af8a
BolleA7X <alessio.bollea@live.it>
2022-09-12 07:02:04 -0400

    Project setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    loop {
+        
+    }
 }
